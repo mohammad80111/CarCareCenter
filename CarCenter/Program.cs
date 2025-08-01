2e90103d5ce156330069c53e5a5ea5b7434c96d5
Mohammad Swlha <mohammad80111@gmail.com>
2025-08-01 16:41:08 +0300

    Fix Program.cs to use dynamic port

--- CarCenter/Program.cs
+++ CarCenter/Program.cs
@@ -1,13 +1,72 @@
+﻿//using CarCenter.Models;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.AspNetCore.Identity;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+////sql
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+//    builder.Configuration.GetConnectionString("MyConn")
+//    ));
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<AppDbContext>();
+
+
+
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+//app.UseAuthentication();;
+
+//app.UseAuthorization();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+
+////identiy
+//app.UseEndpoints(endpoint => endpoint.MapRazorPages());
+
+//app.Run();
+
+
+
+
+
+
+
 using CarCenter.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// قراءة رقم البورت من متغير البيئة PORT، أو استخدام 80 كافتراضي
+var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//sql
+// sql
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("MyConn")
     ));
@@ -15,8 +74,8 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<AppDbContext>();
 
-
-
+// جعل التطبيق يستمع على البورت الديناميكي
+builder.WebHost.UseUrls($"http://*:{port}");
 
 var app = builder.Build();
 
@@ -32,7 +91,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
+app.UseAuthentication();
 
 app.UseAuthorization();
 
@@ -40,10 +99,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
-
-
-//identiy
+// identity
 app.UseEndpoints(endpoint => endpoint.MapRazorPages());
 
 app.Run();
